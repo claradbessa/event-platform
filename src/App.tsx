1e1457d11707dbe63b2457859c306419af90682a
Clara Bessa <clarabessa@outlook.com>
2022-06-22 21:55:23 -0300

    Aula 01 e 02
    estrutura visual do projeto

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { Header } from "./components/Header"
+import { Lesson } from "./components/Lesson"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
+import { client } from "./lib/apollo"
+import { Event } from "./pages/Event"
+
+function App() {
+
+  return (
+   <>
+    <Event />
+   </>
+  )
+}
+
+export default App
