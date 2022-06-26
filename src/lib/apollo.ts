1e8055dc846e505d71fb3a1dfd267c8a0f227b8b
Clara Bessa <clarabessa@outlook.com>
2022-06-26 17:07:18 -0300

    Aula 04
    configurando variáveis ambiente

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o8fa11155101z76eu00htv/master',
-    // uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o8fa11155101z76eu00htv/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}` 
+    },
     cache: new InMemoryCache()
 })
